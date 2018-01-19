6305cd90ae820a1b84a001bcbcd0d12e24e2f6fb
eamirakulov <emil@amirakulov.com>
2018-01-20 05:21:15 +0600

    Rental Services page

---
+++ themes/hunton/page-rentals.php
@@ -0,0 +1,297 @@
+<?php get_header(); ?>
+<div class="page dev2">
+	<div class="container">
+		<div class="page-bg" style="background: url(<?php the_post_thumbnail_url(); ?>) no-repeat top center; background-size: cover;">
+			<div class="text">
+				<h1><?php echo the_title(); ?></h1>
+				<p><?php echo get_field('subheading'); ?></p>
+			</div>
+		</div>
+
+		<div class="why-about-block rental-capabilities-block content-block">
+			<div class="center">
+				<h2 class="bottom-line">Rental Capabilities</h2>
+				<p>Hunton Services Builds value</p>
+			</div>
+			<div class="block-840">
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+				<p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
+				</p>
+			</div>
+
+			<div class="video">
+				<div class="text">
+					<h3>Hunton R'newal</h3>
+					<a href="#video-1" class="modal-trigger play-btn" tabindex="0">
+						<img src="<?php bloginfo('template_url'); ?>/img/play.svg">
+						<img class="rotate" src="<?php bloginfo('template_url'); ?>/img/dottedcircle.svg">
+					</a>
+
+	                <div id="video-1" class="modal">
+	                  <div class="modal-content">
+	                  	<iframe width="640" height="360" src="https://www.youtube.com/embed/rN6nlNC9WQA?feature=oembed&amp;controls=0&amp;hd=1&amp;autohide=1&amp;rel=0&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen=""></iframe>
+	                  </div>
+	                </div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="container">
+		<div class="rental-capabilities-block2 content-block">
+			<div class="center">
+				<h2 class="bottom-line">Rental Capabilities</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+			</div>
+			<div class="coolers row flex">
+				<div class="item col l3">
+					<div class="inner">
+						<img src="<?php bloginfo('template_url'); ?>/img/cooler.png">
+						<h4>Power</h4>
+						<p>Diesel and Natural Gas Generator Rentals</p>
+					</div>
+				</div>
+				<div class="item col l3">
+					<div class="inner">
+						<img src="<?php bloginfo('template_url'); ?>/img/cooler.png">
+						<h4>Cooling</h4>
+						<p>Diesel and Natural Gas Generator Rentals</p>
+					</div>
+				</div>
+				<div class="item col l3">
+					<div class="inner">
+						<img src="<?php bloginfo('template_url'); ?>/img/cooler.png">
+						<h4>Heating</h4>
+						<p>Diesel and Natural Gas Generator Rentals</p>
+					</div>
+				</div>
+				<div class="item col l3">
+					<div class="inner">
+						<img src="<?php bloginfo('template_url'); ?>/img/cooler.png">
+						<h4>Dehumidification</h4>
+						<p>Diesel and Natural Gas Generator Rentals</p>
+					</div>
+				</div>
+				<div class="item col l3">
+					<div class="inner">
+						<img src="<?php bloginfo('template_url'); ?>/img/cooler.png">
+						<h4>Oil-Free Air</h4>
+						<p>Diesel and Natural Gas Generator Rentals</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="container bg-gray">
+		<div class="rental-capabilities-block2 content-block">
+			<div class="center">
+				<h2 class="bottom-line">Rental Applications</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+			</div>
+			<div class="applications">
+				<div class="row flex">
+					<div class="item col m6 l3">
+						<div class="inner">
+							<img src="<?php bloginfo('template_url'); ?>/img/ap-construction.png">
+							<div class="text">
+								<h4>Construction</h4>
+								<p>Ut enim ad minima veniam, quis nostrum exercitationem ullam et suscipit laboriosam...</p>
+							</div>
+						</div>
+					</div>
+					<div class="item col m6 l3">
+						<div class="inner">
+							<img src="<?php bloginfo('template_url'); ?>/img/ap-contracting.png">
+							<div class="text">
+								<h4>Contracting</h4>
+								<p>Ut enim ad minima veniam, quis nostrum exercitationem ullam et suscipit laboriosam...</p>
+							</div>
+						</div>
+					</div>
+					<div class="item col m6 l3">
+						<div class="inner">
+							<img src="<?php bloginfo('template_url'); ?>/img/ap-facilities.png">
+							<div class="text">
+								<h4>Facilities managment</h4>
+								<p>Ut enim ad minima veniam, quis nostrum exercitationem ullam et suscipit laboriosam...</p>
+							</div>
+						</div>
+					</div>
+					<div class="item col m6 l3">
+						<div class="inner">
+							<img src="<?php bloginfo('template_url'); ?>/img/ap-food.png">
+							<div class="text">
+								<h4>Food and beverage</h4>
+								<p>Ut enim ad minima veniam, quis nostrum exercitationem ullam et suscipit laboriosam...</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="container">
+		<section class="contingency content-section">
+			<div class="row flex">
+				<div class="col s12 xl6 center-align"><img src="<?php echo bloginfo('template_url'); ?>/img/contingency.jpg"></div>
+				<div class="col s12 xl6 more-margin">
+					<h2>Contingency Planning<div class="num"><img src="<?php echo bloginfo('template_url'); ?>/img/stat.svg"></h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+
+					<p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+				</div>
+			</div>
+
+			<div class="row steps">
+				<div class="col m6 s12">
+					<div class="row">
+						<div class="col s4"><img src="<?php echo bloginfo('template_url'); ?>/img/step1.svg"></div>
+						<div class="col s8">
+							<h4>Step 1- <span>Financial Risk Analysis</span></h4>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+						</div>
+					</div>
+				</div>
+				<div class="col m6 s12">
+					<div class="row">
+						<div class="col s4"><img src="<?php echo bloginfo('template_url'); ?>/img/step2.svg"></div>
+						<div class="col s8">
+							<h4>Step 2- <span>Risk Assessment </span></h4>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+						</div>
+					</div>
+				</div>
+				<div class="col m6 s12">
+					<div class="row">
+						<div class="col s4"><img src="<?php echo bloginfo('template_url'); ?>/img/step3.svg"></div>
+						<div class="col s8">
+							<h4>Step 3- <span>Equipment Identification </span></h4>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+						</div>
+					</div>
+				</div>
+				<div class="col m6 s12">
+					<div class="row">
+						<div class="col s4"><img src="<?php echo bloginfo('template_url'); ?>/img/step4.svg"></div>
+						<div class="col s8">
+							<h4>Step 4- <span>Prioritization </span></h4>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+						</div>
+					</div>
+				</div>
+				<div class="col m6 s12">
+					<div class="row">
+						<div class="col s4"><img src="<?php echo bloginfo('template_url'); ?>/img/step5.svg"></div>
+						<div class="col s8">
+							<h4>Step 5- <span>System Connection  </span></h4>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section class="emergency content-section">
+			<div class="row flex">
+				<div class="col s12 xl6 more-margin">
+					<h2>Emergency Planning<div class="num"><img src="<?php echo bloginfo('template_url'); ?>/img/stat.svg"></h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+
+					<p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+				</div>
+				<div class="col s12 xl6 center-align"><img src="<?php echo bloginfo('template_url'); ?>/img/maps.jpg"></div>
+			</div>
+			<div class="page-bg" style="background: url(<?php echo bloginfo('template_url'); ?>/img/cta.jpg) no-repeat top center; background-size: cover;">
+				<div class="text">
+					<h3>If you do not find the answer to your question, then contact us. <br> We will help you.</h3>
+					<a href="" class="btn-green btn-large">CONTACT US TODAY!</a>
+				</div>
+			</div>
+		</section>
+		<div class="row flex bottom-box">
+			<div class="col s12 m6 gov">
+				<h4>Kilowatts to amps <br>calculator</h4>
+				<h5>Kilowatts (kW) to amps (A) calculator.</h5>
+				<p>Select current type, enter power in kilowatts, voltage in volts, power factor for AC circuit and press the Calculate button (DC = Direct Current, AC = Alternating Current):</p>
+			</div>
+			<div class="col s12 m6 ips">
+				<div class="inner">
+					<h3>Industrial Plant Services</h3>
+					<p><a href="#">Read more</a></p>
+				</div>
+			</div>
+		</div>
+		<div class="row flex form-box">
+			<div class="col s12 m6 calc">
+				<div class="row cats">
+					<div class="col m3 s12"><h6>Categories:</h6></div>
+					<div class="col m9 s12">
+						<a class="active">kW to amps</a>
+						<a>Amps to Kilowatts</a>
+					</div>
+				</div>
+				<div class="row">
+					<div class="input-field col m8 s12">
+						<select id="type">
+							<option>SELECT CURRENT TYPE</option>
+							<option>1</option>
+							<option>2</option>
+						</select>
+					</div>
+				</div>
+				<div class="row">
+					<div class="input-field col m8 s12">
+						<select id="power">
+							<option>ENTER POWER IN KILOWATTS</option>
+							<option>1</option>
+							<option>2</option>
+						</select>
+					</div>
+				</div>
+				<div class="row">
+					<div class="input-field col m8 s12">
+						<select id="volt">
+							<option>Enter VOLTAGE IN VOLTS</option>
+							<option>1</option>
+							<option>2</option>
+						</select>
+					</div>
+				</div>
+
+				<div class="row">
+					<div class="col s12 m4">
+						<a href="#" id="calc" class="btn-green">CALCULATE</a>
+					</div>
+					<div class="col s12 m4">
+						<a href="#" id="reset" class="btn-or-default">RESET</a>
+					</div>
+				</div>
+
+				<div class="row">
+					<div class="col s12 m8">
+						<input type="text" name="office-find" id="office-find" placeholder="Find an office">
+					</div>
+				</div>
+			</div>
+			<div class="col s12 m6 girlmac">
+				<div class="inner"></div>
+			</div>
+		</div>
+	</div>
+</div>
+<script type="text/javascript">
+    $(document).ready(function(){
+        $('.modal').modal({
+          ready: function(modal, trigger) {
+            var videoSrc = modal.find('iframe').attr("src");
+            modal.find('iframe').attr("src", videoSrc+"&amp;autoplay=1");
+          },
+          complete: function() {
+            $('iframe').attr('src', $('iframe').attr('src').replace("&amp;autoplay=1", ""));
+          }
+        });
+    });
+</script>
+<?php get_footer(); ?>

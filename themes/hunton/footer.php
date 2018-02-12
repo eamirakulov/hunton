b1901345fddeca1e810484791da12dcdf0b158f5
eamirakulov <emil@amirakulov.com>
2018-02-12 07:58:25 +0600

    linked bottom buttons

--- themes/hunton/footer.php
+++ themes/hunton/footer.php
@@ -4,9 +4,9 @@
 				<div class="row">
 					<div class="col s12 m12 l3 copy">© 2017 Hunton Group</div>
 					<div class="col s12 m12 l6 cta">
-						<a href="#" class="btn-default">now hiring</a>
-						<a href="#" class="btn-orange">HOW CAN WE HELP?</a>
-						<a href="#" class="btn-green">RENTALS</a>
+						<a href="<?php bloginfo('url'); ?>/careers" class="btn-default">now hiring</a>
+						<a href="<?php bloginfo('url'); ?>/contact-us" class="btn-orange">HOW CAN WE HELP?</a>
+						<a href="<?php bloginfo('url'); ?>/rentals" class="btn-green">RENTALS</a>
 					</div>
 					<div class="col s12 m12 l3 phone">
 						<a href="#"></a> 713 643-8336
